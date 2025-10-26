e4dd15d5a7469406b3c4bbf61eed0d921d03c30e
Kalena Lam <kalenalam22@gmail.com>
2025-10-26 12:49:45 -0600

    it runssss; setp up CS464GroupProject Swift package w/o tests

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CS464GroupProject",
+    platforms: [
+        .macOS(.v12)
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+
+        .executableTarget(
+            name: "CS464GroupProject",
+            dependencies: []),
+    ]
+)
